859c678d1479b25127106f69af39d51bfe33c7e7
vector-gru <popelouis1@outlook.com>
2026-08-04 00:41:38 +0100

    fix: app bundle issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.hifiveteck.neno-smartlife"
+        applicationId = "com.hifiveteck.neno_smartlife"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
